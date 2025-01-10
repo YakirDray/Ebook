ae9505a8fef97a3ae18961a1f3acec4dfa1aa627
YakirDray <158854050+YakirDray@users.noreply.github.com>
2025-01-10 09:03:19 +0200

    h

--- obj/Debug/net8.0/MyEBookLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyEBookLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEBookLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8ca1487cab0235756070a2ea56a44b0ef0323f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fec4ca111c46747bb8f7859e9ee144a05f9dca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEBookLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEBookLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
