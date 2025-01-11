a4f61550d281d6ab9ebeea6fcc9ee402a953bb0a
topi2244 <topazt2244@gmail.com>
2025-01-11 22:51:18 +0200

    BOOKS DB FIXED

--- obj/Debug/net8.0/MyEBookLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyEBookLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEBookLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca33d698ff42140ecb2b307cc22980ad792145fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd01d1c7894df31c03c61440df1f43dd3057957")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEBookLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEBookLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
